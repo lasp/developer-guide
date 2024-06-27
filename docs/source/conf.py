79140277bb105d3597307633fadfd689dfd11b6e
Matthew Bourque <matthew.bourque@lasp.colorado.edu>
2024-06-27 11:33:01 -0600

    Fixed myst_parser dependency

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ release = '0.1.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'myst_parser',
+    'myst-parser',
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
